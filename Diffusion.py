7156618633a2e06031f88c2a4d4178161ddd464f
WilliamTaing <53840760+WilliamTaing@users.noreply.github.com>
2020-02-04 16:40:38 -0800

    Add files via upload
    Diffusion files

--- Diffusion.py
+++ Diffusion.py
@@ -1,4 +1,4 @@
-__author__ = 'v-caearl'
+#William Taing CSS 458
 
 def diffusion(diffusionRate, site, N, NE, E, SE, S, SW, W, NW):
     return(1-8*diffusionRate)*site + diffusionRate*(N + NE + E +  SE + S + SW + W + NW)
---
+++ DiffusionExtended.py
@@ -0,0 +1,15 @@
+#William Taing CSS 458
+
+#Preconditions: lat is a grid
+#Postconditions: lat has changed due to diffusion
+def diffusionExtended(m, n, latExt):
+    #Create lat, a lattice of latExt wihout the extended boundaries
+    lat = [[]]
+    
+    for i in range(1, m):
+        for j in range(1,n):
+            lat[i-1][j-1].append(diffusion(diffusionRate, latExt[i][j],\
+            latExt[i-1][j], latExt[i-1][j+1], latExt[i][j+1], latExt[i+1][j+1],\
+            latExt[i+1][j], latExt[i+1][j-1], latExt[i][j-1], latExt[i-1][j-1])      
+    return lat
+            
