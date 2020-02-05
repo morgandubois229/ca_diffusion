226900ee8e08c4b2a5c5fc2f2310f00505baf43d
Christian M. Rahmel <35943405+OmeganKryist@users.noreply.github.com>
2020-02-04 16:25:16 -0800

    Update reflectingLat.py

--- reflectingLat.py
+++ reflectingLat.py
@@ -1,1 +1,8 @@
 def reflectingLat(lat):
+    shape = nu.shape(lat)
+    relat = nu.zeros((shape[0]+2, shape[1]))
+    
+    relat[0, :] = lat[0, :]
+    relat[1:shape[0]+1, :] = lat
+    relat[shape[0]+1, :] = lat[shape[0]-1, :]
+    return relat
